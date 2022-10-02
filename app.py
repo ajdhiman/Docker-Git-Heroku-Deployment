c069a7dbf0243568a175bbf9f2ff4069e39c03c9
Ajay <er.ajaydhiman@ymail.com>
2022-10-02 17:09:52 +0530

    Adding files

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("Project has started.")
+    
+    try:
+        raise Exception("Custom exception raised on purpose")
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+
+
     return "Hi there! Project working with CI/CD pipelines."
 
 
