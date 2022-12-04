40a6862066f70b46ac3daf737659246255fdf00e
Ajay <er.ajaydhiman@ymail.com>
2022-12-04 22:37:55 +0530

    Adding files

--- demo.py
+++ demo.py
@@ -1,16 +1,16 @@
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
-
-
+from housing.config.configuration import Configuration
+import os
 def main():
     try:
 
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
-
+        config_path = os.path.join("config", "config.yaml")
+        pipeline = Pipeline(Configuration(config_file_path=config_path))
+        pipeline.start()
         logging.info("main function execution completed.")
-
+        # pipeline.run_pipeline()
     except Exception as e:
         logging.error(f"{e}")
         print(e)
